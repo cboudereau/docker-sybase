1d90086682dd61113676acc1fc2f05aa5a8942c7
Clément Boudereau <cboudereau@gmail.com>
2022-10-25 03:37:43 +0200

    undo failure

--- .ci/test.sh
+++ .ci/test.sh
@@ -7,7 +7,7 @@ echo Run database 1-table.sql init file assertion; grep -e "running /docker-entr
 
 echo Run database 2-data.sql.gz init file assertion; grep -e "running /docker-entrypoint-initdb.d/2-data.sql.gz" /log/database.log && \
     
-echo Run database should_be_ignored.txt ignored file assertion; grep -e "ignoring /docker-entrypoint-initdb.d/4-should_be_ignored-failed.txt" /log/database.log && \
+echo Run database should_be_ignored.txt ignored file assertion; grep -e "ignoring /docker-entrypoint-initdb.d/4-should_be_ignored.txt" /log/database.log && \
      
 echo Run Select assertions; grep -e "IDROW                 TEST_FIELD1" /log/database.log && \
                             grep -e "--------------------- -----------" /log/database.log && \
