fc33adfb63e61cc435c9bb50281c7539985b4a7b
Clément Boudereau <cboudereau@gmail.com>
2022-10-25 08:43:51 +0200

    test failure

--- .ci/wait-for-file.sh
+++ .ci/wait-for-file.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -eu
+waitForFile() {
+    local f=$1
+    
+    while  [[ ! -f "$f" ]]
+    do
+        echo "waiting $f"
+        sleep 1
+    done
+    cat $f
+}
 
-f=$1
-while  [[ ! -f "$f" ]]
-do
-    echo "waiting $f"
-    sleep 1
-done
-cat $f
+waitForFile $@
