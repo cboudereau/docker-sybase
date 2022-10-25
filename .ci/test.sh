4b1bf33ad0e42da6ed8b11ea00a2032de1594eb3
Clément Boudereau <cboudereau@gmail.com>
2022-10-25 09:40:29 +0200

    test failure

--- .ci/test.sh
+++ .ci/test.sh
@@ -8,7 +8,7 @@ echo Test database 1-table.sql init file assertion && grep -e "running /docker-e
 
 echo Test database 2-data.sql.gz init file assertion && grep -e "running /docker-entrypoint-initdb.d/2-data.sql.gz" /log/database.log;
     
-echo Test database should_be_ignored.txt ignored file assertion && grep -e "ignoring /docker-entrypoint-initdb.d/4-failed-should_be_ignored.txt" /log/database2.log;
+echo Test database should_be_ignored.txt ignored file assertion && grep -e "ignoring /docker-entrypoint-initdb.d/4-failed-should_be_ignored.txt" /log/database.log;
      
 echo Test Select assertions && grep -e "IDROW                 TEST_FIELD1" /log/database.log && \
                                grep -e "--------------------- -----------" /log/database.log && \
