a6bbb752e6f505e2f063b042b277565f9c310c83
Clément Boudereau <cboudereau@gmail.com>
2022-10-26 15:54:35 +0200

    test: add sh script test

--- .ci/test/test.sh
+++ .ci/test/test.sh
@@ -6,6 +6,11 @@ set -eu
 waitForFile /log/done;
 
 echo Test database create assertion && grep -e "Database 'TESTDB' is now online" /log/database.log;
+
+echo Test database 0-hello.sh assertion && \
+    grep -e "running /docker-entrypoint-initdb.d/0-hello.sh" /log/database.log && \
+    grep -e "hello world" /log/database.log;
+
 echo Test database 1-table.sql init file assertion && grep -e "running /docker-entrypoint-initdb.d/1-table.sql" /log/database.log;
 
 echo Test database 2-data.sql.gz init file assertion && grep -e "running /docker-entrypoint-initdb.d/2-data.sql.gz" /log/database.log;
