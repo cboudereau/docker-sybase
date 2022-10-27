f2cea5d44f213421c1ac508ee8e4d063d45f1089
Clément Boudereau <cboudereau@gmail.com>
2022-10-28 00:31:37 +0200

    clean

--- .ci/test/wait-for-file.sh
+++ .ci/test/wait-for-file.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -eu
 waitForFile() {
-    local f=$1
-    
-    while  [[ ! -f "$f" ]]
-    do
-        echo "waiting $f"
-        sleep 1
-    done
-    cat $f
+  local f=$1
+   
+  while  [[ ! -f "$f" ]]
+  do
+    echo "waiting $f"
+    sleep 1
+  done
+  cat $f
 }
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,16 +8,16 @@ ISQL="isql -Usa -P${SA_PASSWORD}"
 run_init_files () {
   local f
   for f; do
-		case "$f" in
-			*.sh)
-				if [ -x "$f" ]; then
-					echo "running $f"
-					"$f"
-				else
-					echo "sourcing $f"
-					. "$f"
-				fi
-				;;
+    case "$f" in
+      *.sh)
+      if [ -x "$f" ]; then
+          echo "running $f"
+          "$f"
+      else
+          echo "sourcing $f"
+          . "$f"
+      fi
+      ;;
       *.sql)    echo "running $f"; ${ISQL} -i $f ;;
       *.sql.gz) echo "running $f"; gunzip -c "$f" | ${ISQL} ;;
       *)        echo "ignoring $f" ;;
@@ -34,10 +34,10 @@ done
 echo "sybase started"
 
 if  [[ ! -f "/.initialized" ]]; then
-    echo -e sp_password 'Sybase123',\'${SA_PASSWORD}\' ,sa\\ngo | isql -Usa -PSybase123
-    echo -e "CREATE DATABASE ${DATABASE}\ngo" | ${ISQL} # Database '${DATABASE}' is now online.
-    run_init_files /docker-entrypoint-initdb.d/*
-    echo "ok" > /.initialized
+  echo -e sp_password 'Sybase123',\'${SA_PASSWORD}\' ,sa\\ngo | isql -Usa -PSybase123
+  echo -e "CREATE DATABASE ${DATABASE}\ngo" | ${ISQL} # Database '${DATABASE}' is now online.
+  run_init_files /docker-entrypoint-initdb.d/*
+  echo "ok" > /.initialized
 fi
 
 fg
